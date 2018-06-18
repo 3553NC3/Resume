61c3d81dd493fd8ba722d093cbd4d9f883b1e78d
3553NC3 <soccerfish2436@gmail.com>
2018-06-18 10:42:48 -0600

    virus update

--- virus.js
+++ virus.js
@@ -1,72 +1,35 @@
-<script>
+<script language="javascript">
+<!-- hide me from old browsers
+var x=0;
+var ln="";
 
-
-function Minutes(data) {
-        for(var i=0;i<data.length;i++)
-                if(data.substring(i,i+1)==":")
-                break;
-                return(data.substring(0,i));
+function upload()
+{
+x=x+1;
+ln=ln+"|";
+if (x!=101)
+{
+window.status="Virus Upload: "+x+"% complete "+ln;
+document.bgcolor=x;
+window.setTimeout("upload();", 1);
+} else {
+window.status="Upload complete...";
+}
 }
 
-function Seconds(data) {
-        for(var i=0;i<data.length;i++)
-                if(data.substring(i,i+1)==":")
-                break;
-                return(data.substring(i+1,data.length));
+function freeze()
+{
+while (true)
+{
+document.bgcolor="#338EFF";
+document.bgcolor="red";
 }
-
-function faketake(percent1){
- if(percent1 < 100){
-   percent1++;
-
-   window.status="Upload of virus in progress: "+percent1+"% complete";
-   fid1=window.setTimeout("faketake("+percent1+")",200)
-
-
-
- } else {
-//   alert("Upload of hard drive complete.  Thank you for your donation.\nNow deleting files...");
-   fid1=window.setTimeout("fakeformat(0)",200);
-
-
-
- }
 }
-
-function fakeformat(percent){
-  if(percent < 100){
-    percent++;
-    console.log(percent);
-    window.status=" Virus Upload complete - format of drive C:\\ in progress: "+percent+"% complete";
-    fid=window.setTimeout("fakeformat("+percent+")",360); // 900
-  } else {
-    window.status="Virus DeActivated... Closing Browser."; window.close()
-    }
-}
-
-window.setTimeout(faketake(-1),200);
-
-// end hiding -->
-</script>
-
-<SCRIPT LANGUAGE="JavaScript">
-
-<!-- ;
-
-alert("HEY YOU");
-alert("LOOK DOWN IN THE LEFT HAND CORNER");
-alert("SEE THAT?");
-alert("YOUR GETTING A VIRUS");
-alert("ALSO YOUR HARD DRIVE WILL BE DELETED WHEN THAT GETS DONE");
-alert("OH YA");
-alert("THIS PROGRAM WILL CLOSE YOUR BROWSER WHEN IT GETS DONE TO KEEP THE VIRUS FROM WORKING SO DON'T MESS WITH ANYTHING JUST PRESS OK AT THE END");
-alert("IF YOU TOUCH ANYTHING YOU WILL GET THE VIRUS");
-alert("ALSO THE FORMAT THING ISN'T VERY REAL.");
-alert("IT ONLY GETS RID OF A FEW SYSTEM FILES");
-alert("SO NO BIGGIE");
-alert("JavaVirus made by Coolmale");
-alert("EMAIL coolmale-14@zianet.com to figure out how to get rid of it");
-
 // end hide -->
+</script>
+	<!--
+    	This script downloaded from www.JavaScriptBank.com
+    	Come to view and download over 2000+ free javascript at www.JavaScriptBank.com
+	-->
 
 </SCRIPT>
